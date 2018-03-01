33b33f394060e8dd01a10e69d0a5453f08a561c5
hui <1378959973@qq.com>
2018-03-01 15:30:22 +0800

    first commit

---
+++ china.py
@@ -0,0 +1,114 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import xlwt
+import datetime
+import time
+import random
+import threading
+def findCity(url):
+    Target = url;
+    Target0 = "http://www.xzqy.net/"
+    headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36'}
+    ip=getip()
+    rep = requests.get(url=Target,headers=headers,proxies=ip)
+    text = rep.text;
+    soup = BeautifulSoup(text, "html5lib")
+    city = str(soup.find_all('td', class_="parent")).split('<a')
+    pattern = re.compile('"./(.*htm)"')
+    pattern1 = re.compile('>(.*)</a>')
+    del city[0]
+
+
+    cityUrl = [];
+    cityName = [];
+    for i in range(len(city)):
+        cityU = Target0 + pattern.findall(city[i])[0]
+        name = pattern1.findall(city[i])[0]  # 省名
+        cityUrl.append(cityU)
+        cityName.append(name)
+    return [cityUrl, cityName]
+
+
+def getMessage(url):
+    Target = url
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36'}
+    ip = getip()
+    rep = requests.get(url=Target, headers=headers, proxies=ip)
+    text = rep.text;
+    soup = BeautifulSoup(text, "html5lib")
+    city = str(soup.find_all('td', class_="parent")).split('<a')
+    pattern = re.compile('"./(.*htm)"')
+    pattern1 = re.compile('>(.*)</a>')
+    del city[0]
+
+    message = []
+    for i in range(len(city)):
+        target2 = pattern1.findall(city[i])[0]  # 省名
+        message.append(target2)
+    return message
+def getip():
+    f = open("G://ip.txt", 'r')
+    lines = f.readlines()
+    proxys = []
+    for i in range(0, len(lines)):
+        ip = lines[i]
+        proxy = 'http:\\' + ip
+        proxies = {'proxy': proxy}
+        proxys.append(proxies)
+    pro=random.choice(proxys);
+    f.close()
+    return pro
+
+def province(url,Pname):
+
+    city = [];
+    name = [];
+    [city, name] = findCity(url)
+    wrd = xlwt.Workbook()
+    sheet = wrd.add_sheet(Pname)
+    CityRow = 0;
+    row = 0;
+    print("即将爬取"+Pname+"数据*************************************")
+    for i in range(len(name)):
+        sheet.write(CityRow, 0, name[i])
+
+        [countryU, CountryName] = findCity(city[i])
+        print("爬取" + name[i] + "数据中")
+
+        for j in range(len(CountryName)):
+            sheet.write(row, 1, CountryName[j])
+            message = getMessage(countryU[j])
+            print('正在爬取' + CountryName[j] + "数据")
+
+            sss="、".join(message)
+            sheet.write(row,2,sss)
+            print(CountryName[j] + "数据写入完成")
+            row = row + 1;
+        print(name[i] + "数据写入完成")
+        CityRow = CityRow + len(CountryName);
+    wrd.save("G://"+Pname+".xls")
+    print("爬取完成")
+
+Target="http://www.xzqy.net/"
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36'}
+ip = getip()
+rep = requests.get(url=Target, headers=headers, proxies=ip)
+
+text=rep.text;
+soup=BeautifulSoup(text, "html5lib")
+province0=str(soup.find_all('div',class_="navi")).split('</a>')
+pattern = re.compile('./(.*)"')
+pattern1=re.compile('>(.*)')
+del province0[0]
+del province0[0]
+del province0[-1]
+for i in range(0,len(province0)):
+    target1=Target+pattern.findall(province0[i])[0]
+    target2=pattern1.findall(province0[i])[0]#省名
+    province(target1,target2)
+    print(type(target2));
+
